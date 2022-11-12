4de57b52c70202514b7680351e3a8ac9c440409b
geekbrains <info@geekbrains.ru>
2022-11-12 20:56:30 +0500

    task2

---
+++ task2/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите первое число 1 : ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите первое число 2 : ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 > number2)
+{
+    Console.WriteLine("Первое число больше,второе меньше");
+}
+else
+{
+    Console.WriteLine("Первое число меньше,второе больше");
+}
