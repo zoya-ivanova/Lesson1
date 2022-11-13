4ed952dec1d53cb75e2923c11528e24d7a2962e0
geekbrains <info@geekbrains.ru>
2022-11-13 12:55:41 +0500

    task4

---
+++ task4/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите первое число 1: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число 2: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число 3: ");
+int number3 = Convert.ToInt32(Console.ReadLine());
+
+int max  = number1;
+
+if (number1 > max) max = number1;
+if (number2 > max) max = number2;
+if (number3 > max) max = number3;
+
+Console.WriteLine("max = ");
+Console.WriteLine(max);
