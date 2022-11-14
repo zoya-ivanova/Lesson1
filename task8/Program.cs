8dba31f74cdbef56a26a43f6fb6f5d240f3208e2
geekbrains <info@geekbrains.ru>
2022-11-14 18:54:31 +0500

    task8

---
+++ task8/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int b = 1;
+
+while (b <= n)
+{
+    if (b % 2 == 0)
+    Console.WriteLine($"четное число {b}");
+    b++;
+
+}
+
